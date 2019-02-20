a0d212cb34549d7beb7f76e20d2b3ebd3259d839
Sophia Yao <sy2743@columbia.edu>
2019-02-20 16:15:08 -0500

    added file for fuzzers

---
+++ IP-fuzzer.py
@@ -0,0 +1,7 @@
+class IPFuzzer():
+    def __init__(self,source,dest,payload=None,fields=[]):
+        self._source = source
+        self._dest = dest
+        self._payload = payload
+        self._fields = 
+    def create_packets(self):
