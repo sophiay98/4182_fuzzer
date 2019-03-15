04087f24d1f2ac7671b864c745d8881c5c5b6d09
Sophia Yao <sy2743@columbia.edu>
2019-03-14 21:59:26 -0400

    function for setting the payload.

--- IP-fuzzer.py
+++ IP-fuzzer.py
@@ -9,43 +9,70 @@ class IPFuzzer():
         self._field_val_map = {"len":"0xffff","proto":"0xff","ihl":"0xf",
                                "flags":"0b111","frag":"0b1111111111111",
                                "ttl":"0xff",}
-
+        self._payload_addr = "./payload"
         #"source", "TOS", "id", "checksum"
-        self._packets = []
 
 
+    def _get_payload(self):
+        try:
+            f = open(self._payload_addr,"r")
+            paylaods = f.readlines()
+            f.close()
+            #TODO : restriction on the length of the payload?
+            return paylaods[0]
+        except IOError:
+            f = open(self._payload_addr, "w")
+            payload = "default payload"
+            f.write(payload)
+            f.close()
+            return payload
 
     def _fuzz_from_file(self,file):
-        pass
+        pckts = []
+
+        try:
+            f = open("./"+file,"r")
+            pckts_info = f.readlines()
+            #TODO: Confirm if it can be CSV format
+
+        except IOError:
+            print("Cannot open file, running default fuzzing test instead...")
+            pckts = self._fuzz_by_fields()
+
+        return pckts
+
+    def _fuzz_by_fields(self, fields=None):
+        pckts = []
+        special_fields = set()
+
+        if not fields:
+            fields = self._field_val_map.keys()
+
+        # need to modify for certain fields.
+
+        for field in fields:
+            ip = IP(dst=self._dest)  # create default packet
+
+            if field in special_fields:
+                pass  # do something special
+            else:
+                trial = range(int(self._field_val_map[field], 0) + 1)
+
+            for _ in trial:
+                setattr(ip, field, _)
+                pckts.append(ip/TCP()/RAW(load=self._get_payload()))
+        return pckts
 
 
     def fuzz(self, fields=None, file=None):
 
-        special_fields =set()
-
-
         if file:
-            self._fuzz_from_file(file)
+            pckts = self._fuzz_from_file(file)
 
         else:
-            #for auto testing. or do an auto function?
-            if not fields:
-                fields = self._field_val_map.keys()
+            pckts = self._fuzz_by_fields(fields)
 
-            #need to modify for certain fields.
-            for field in fields:
-                ip = IP(dst=self._dest) #create default packet
-
-                if field in special_fields:
-                    pass    #do something special
-                else:
-                    trial = range(int(self._field_val_map[field],0)+1)
-
-                for _ in trial:
-                    setattr(ip,field,_)
-                    self._packets.append(ip)
-
-        for pckt in self._packets:
+        for pckt in pckts:
             sendp(Ether()/pckt)
 
 
