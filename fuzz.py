2a9410c2db7a500b037d4e8b03febf6070a68f98
Yoongbok Lee <asdf>
2019-03-23 03:01:46 -0400

    better argparsing
    Signed-off-by: Yoongbok Lee <asdf>

--- fuzz.py
+++ fuzz.py
@@ -3,14 +3,15 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Fuzzing IP, Transport(TCP), Payloads with scapy.')
+    parser.add_argument('-F', '--file', action='store', dest='file_name', default='default.csv',
+                        help='select file to read the fuzzing data')
     parser.add_argument('-I', '--ip', action='store_true', default=False,
                         help='run fuzzing for IP layer')
     parser.add_argument('-T', '--tcp', action='store_true', default=False,
                         help='run fuzzing for TCP layer')
     parser.add_argument('-P', '--payload', action='store_true', default=False,
                         help='run fuzzing for Payloads')
-    parser.add_argument('-F', '--file', action='store', dest='file_name', default='default.csv',
-                        help='select file to read the fuzzing data')
+
     args = parser.parse_args()
     print(args)
     print(args.file_name)
