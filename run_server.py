f74fa31c652685c67979ddf9695870745ef520a5
Yoongbok Lee <asdf>
2019-03-25 08:48:44 -0400

    completed server end

--- run_server.py
+++ run_server.py
@@ -1,30 +1,57 @@
+
 import socket
 from scapy.all import *
 
-
-class Reservation(Packet):
-    name = "ReservationPacket"
-    fields_desc=[ ByteField("id", 0),
-        BitField("type",None, 0),
-        X3BytesField("update", 0),
-        ByteField("rssiap", 0)]
+ip = '192.168.1.11'
+port = 1338
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('192.168.240.1', 5000))
-s.listen(1)
 
-while True :
 
-    conn, addr = s.accept()
+def sniff(self):
+    s = L3RawSocket()
+    while self.connected:
+        p = s.recv(MTU)
+        if p.haslayer(TCP) and p.haslayer(Raw) \
+                and p[TCP].dport == self.sport:
+            self.do_ack(p)
+        if p.haslayer(TCP) and p[TCP].dport == self.sport \
+                and p[TCP].flags & 0x01 == 0x01:  # FIN
+            self.ack_rclose()
 
-    print ('Connection address:', addr)
+    s.close()
+    self._ackThread = None
 
-    print ('')
 
-    data = conn.recv(1024)
-    print(data)
-    conn.close()
-
-s.close()
+sock = L3RawSocket()
+print("server ready!")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    while True:
+        try:
+            s.bind((ip, int(port)))
+            break
+        except Exception as e:
+            continue
+    s.listen(1)
+    print("Listening on port: " + str(port))
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print('Connection address:', addr)
+            data = ''
+            while conn:
+                data += str(conn.recv(1024))
+                print(data)
+                if pattern in data:
+                    conn.send(b"0x00")
+                else:
+                    conn.send(b"0xff")
+                if not data:
+                    break
+                if not data.endswith('\r\n'):
+                    continue
+                lines = data.split('\r\n')
+                for line in lines:
+                    print(line)
+                data = ''
