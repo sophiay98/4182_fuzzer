1dbef598908b1676425e4cbd9dcb59eb4d99d83a
Sophia Yao <sy2743@columbia.edu>
2019-03-27 16:06:19 -0400

    sending packets with payload function

--- src/app_fuzzer.py
+++ src/app_fuzzer.py
@@ -1,8 +1,9 @@
 from scapy.all import *
 
+
 class APPFuzzer():
-    def __int__(self,source,dest,addr=None):
-        self._pckt = Ether()/IP(dst=dest,source=source)/TCP()
+    def __init__(self,source="127.0.0.1", dest="127.0.0.1"):
+        self._pckt = Ether()/IP(dst=dest,src=source)/TCP()
 
     def _get_payload(self,addr):
         pckts = []
@@ -29,12 +30,14 @@ class APPFuzzer():
 
     def _rand_payload(self,test=0,size=None,min_len=2,max_len=128):
         pckts = []
+        if size:
+            size=size*2
         for _ in range(test):
             length=size
             if not size:
                 length = random.randint(min_len, max_len)*2
             payload = ''.join(
-                random.choice(list(chr(_) for _ in range(ord('a'), ord('f') + 1)) + list(range(10))) for _ in
+                random.choice(list(chr(_) for _ in range(ord('a'), ord('f') + 1)) + list(str(_) for _ in range(10))) for _ in
                 range(length))
             pckts.append(self._pckt / bytes.fromhex(payload))
         return pckts
@@ -45,5 +48,9 @@ class APPFuzzer():
         else:
             pckts = self._rand_payload(test,size,min_len,max_len)
 
+        for pckt in pckts:
+            sendp(pckt)
 
 
+fuzz = APPFuzzer("127.0.0.1","127.0.0.1")
+fuzz.fuzz(test=3)
